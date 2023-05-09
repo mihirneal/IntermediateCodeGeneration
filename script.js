796a62553882ac885c50c82ab3eb1b70f7ac5cc1
Mihir Neal <45179700+mihirneal@users.noreply.github.com>
2023-05-09 03:18:45 -0400

    bug fixes

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
-function generateIntermediateCode() {
-    var cppCode = document.getElementById("cpp-code").value;
-
-    // Send the C++ code to the server for intermediate code generation
-    fetch("/generate", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ code: cppCode })
-    })
-    .then(response => response.text())
-    .then(intermediateCode => {
-        // Display the intermediate code in the browser
-        document.getElementById("intermediate-code").textContent = intermediateCode;
-    })
-    .catch(error => console.error(error));
-}
+$(document).ready(function() {
+    $('form').on('submit', function(event) {
+      event.preventDefault();
+  
+      var cppCode = $('textarea[name="cpp_code"]').val();
+  
+      $.post('/generate', { cpp_code: cppCode }, function(response) {
+        $('#intermediate-code').html(response);
+      });
+    });
+  });
+  
